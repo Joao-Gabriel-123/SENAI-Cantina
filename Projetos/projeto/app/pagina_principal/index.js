1eb4a0161a9b1844312c0542f00927a71a1de288
João <jgablopes2008@gmail.com>
2025-05-12 07:50:17 -0300

    inserção do projeto

---
+++ Projetos/projeto/app/pagina_principal/index.js
@@ -0,0 +1,11 @@
+function AbrirMenu(){
+
+    if (document.getElementById('menu').style.visibility == 'hidden'){
+        document.getElementById('menu').style.visibility = 'visible'
+    
+    } else{
+        document.getElementById('menu').style.visibility = 'hidden'
+    
+    }
+    
+}
