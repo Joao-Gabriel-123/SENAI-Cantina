b747f8f9503bf4cbdc2e79f679af241343cb4bd2
João <jgablopes2008@gmail.com>
2025-05-12 11:35:57 -0300

    ajustes na lógica

--- Projetos/projeto/app/tela_cadastro/script.js
+++ Projetos/projeto/app/tela_cadastro/script.js
@@ -14,11 +14,11 @@ function CalcularLucro() {
     let preco_custo = document.getElementById('preco-custo').value
     let preco_venda = document.getElementById('preco-venda').value
 
-    if (preco_custo != '' || preco_venda != '') {
+    if (preco_custo != '' && preco_venda != '') {
 
         if (preco_custo == preco_venda) {
             document.getElementById('lucro').value = `0%`
-
+            return
         }
 
         if (preco_venda > preco_custo) {
@@ -33,6 +33,9 @@ function CalcularLucro() {
             document.getElementById('lucro').value = `-${lucro}%`
 
         }
+
+    } else {
+        document.getElementById('lucro').value = ``
 
     }
 
